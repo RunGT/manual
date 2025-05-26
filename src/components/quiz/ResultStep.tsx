6eda4ec7d290d9641bdf4a025819609412f92a72
Glenroy-Terence Abdullah <terryabdullah@lickd.co>
2025-05-26 05:59:06 +0100

    Chore: add comment

--- src/components/quiz/ResultStep.tsx
+++ src/components/quiz/ResultStep.tsx
@@ -9,6 +9,7 @@ export default function ResultStep({ rejected }: Props) {
   return (
     <Container>
       <ResultText>
+        {/* Note: The rejected text below isn't always relevant to a rejection response from the API. To improve UX we would want to return custom responses from the API based on the user's answers, but for now we are using a generic rejection message from the tech test requirements. */}
         {rejected ? (
           "Unfortunately, we are unable to prescribe this medication for you. This is because finasteride can alter the PSA levels, which may be used to monitor for cancer. You should discuss this further with your GP or specialist if you would still like this medication."
         ) : (
